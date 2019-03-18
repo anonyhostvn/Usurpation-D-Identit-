b2e18183b6c1ec3b61077c6919fa201d52712414
anonyhostvn <anonyhostvn@gmail.com>
2019-03-18 18:16:18 +0700

    add image and fix some effect

--- page.js
+++ page.js
@@ -2,6 +2,8 @@ let color ;
 
 $('.high-light').hover (function() {
     $(this).animate({backgroundColor: '#000000'}, 400) ;
+    $(this).find("div").find("#image-need-fade").animate({opacity : 0.7 } , 400 ) ; 
 } , function() {
     $(this).animate({backgroundColor: '#494949'}, 100) ;
+    $(this).find("div").find("#image-need-fade").animate({opacity : 0.0 } , 100 ) ; 
 }) ; 
